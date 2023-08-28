44d11552233baf6d5dd1cf3fc0130b1e06475b41
Brent Rogers <brentarogers@gmail.com>
2023-08-28 10:23:20 -0400

    add aws details

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "tags" {
+  description = "Tags to put on everything"
+  type        = map(string)
+  default = {
+    project = "jitsi-terraform"
+  }
+
+}
